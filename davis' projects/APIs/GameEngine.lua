14b4d0155f6e6d13eb10adbab9a81b0115c6990e
RobertTeaches <77590088+RobertTeaches@users.noreply.github.com>
2022-07-26 17:56:17 -0400

    Fixed GameEngine

--- davis' projects/APIs/GameEngine.lua
+++ davis' projects/APIs/GameEngine.lua
@@ -6,7 +6,9 @@ _onUpdate_Functions = {}
 
 --Main Loop
 local function gameLoop()
-    while sleep(0.1) do
+
+    while true do
+        sleep(0.1)
         for i, v in pairs(_onUpdate_Functions) do
             v()
         end
@@ -14,7 +16,7 @@ local function gameLoop()
 end
 --Key Loop
 local function keyDownInput()
-    while true do 
+    while true do
         local e, key = os.pullEvent("key")
         pressedKeys[keys.getName(key)] = true
     end
@@ -23,17 +25,18 @@ local function keyUpInput()
    while true do
     local e, key = os.pullEvent("key_up")
         pressedKeys[keys.getName(key)] = false
-   end 
+   end
 end
 
 local hasStarted = false
 function init()
     if hasStarted then return end
+
     parallel.waitForAny(gameLoop, keyDownInput, keyUpInput)
     hasStarted = true
     --We choose this range due to the number range provided for keyCodes here:
     -- https://github.com/cc-tweaked/CC-Tweaked/blob/ebef3117f201aaece14b9ac6a58d75e671456acf/src/main/resources/data/computercraft/lua/rom/apis/keys.lua#L149
-    for i in 1,350 do
+    for i = 1,350 do
         if keys.getName(i) then
             pressedKeys[keys.getName(i)] = false
         end
