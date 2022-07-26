cadfbd2b119d5526c2b58797d736ba00f4efcecd
davisw-ST <davisw@sigamteaches.com>
2022-07-26 17:12:16 -0400

    Updated STGPS.lua
    STGPS API is now functional and can store PC's to its table and send GPS locations to requesting computers.

--- davis' projects/APIs/STGPS.lua
+++ davis' projects/APIs/STGPS.lua
@@ -23,7 +23,7 @@ function GetGlobalChannel()
     return GlobalChannel
 end
 
-function Position(id)
+function sendPosition(id)
     local computer = {}
     x,y,z = gps.locate()
     computer["x"] = x
@@ -40,26 +40,40 @@ function StartGlobalGPS()
         --print("Waiting for a message")
         local event, side, channel, replyChannel, payload, distance = os.pullEvent("modem_message")
         
-        if payload.action == "receiveMyPosition" then
-            --print("Added user to the table")
-            users[payload.ID] = payload
-            userCount = userCount+1
-        elseif payload.action == "getComputerPosition" then
-            print("Getting computer ".. payload.ID .. "'s location")
-            target = {}
-            for k,v in pairs(users) do
-                if tonumber(payload.ID) == v.ID then
-                    target = users[k]
-                end
-            end
-            sleep(2)
-            print("Sending user " .. target.ID .. "'s position to " .. replyChannel)
-            modem.transmit(replyChannel,GlobalChannel,target)
-        end
+        handleEvent(payload,replyChannel)
 
         print("Current User Count: " .. userCount)
         for k,v in pairs(users) do
             print("User " .. v.ID .. " - X: " .. v.x .. " Y: " .. v.y .. " Z: " .. v.z)
+        end
+    end
+end
+
+function handleEvent(payload,replyChannel)
+    --Send Computer Position to Global GPS Server
+    if payload.action == "receiveMyPosition" then
+        --print("Added user to the table")
+        if users[payload.ID] == nil then
+            print("New user position received.")
+            userCount = userCount+1
+        end
+        print("Adding position to table")
+        users[payload.ID] = payload
+    --Request the position of another PC that is located on the network.
+    elseif payload.action == "getComputerPosition" then
+        print("Getting computer ".. payload.ID .. "'s location")
+        target = {}
+        for k,v in pairs(users) do
+            if tonumber(payload.ID) == v.ID then
+                target = users[k]
+            end
+        end
+        if target == nil then
+            print("Could not find computer with ID: " .. payload.ID)
+        else
+            sleep(2)
+            print("Sending user " .. target.ID .. "'s position to " .. replyChannel)
+            modem.transmit(replyChannel,GlobalChannel,target)
         end
     end
 end
