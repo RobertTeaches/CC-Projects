7d085bb5c772ac83c7090f42dea279d04eae5ada
RobertTeaches <77590088+RobertTeaches@users.noreply.github.com>
2022-07-30 16:31:31 -0400

    Updated to utilize ezTurtle api a lot more, cutting down ~100 lines of redundant code

--- apis/ezTurtle.lua
+++ apis/ezTurtle.lua
@@ -17,3 +17,24 @@ function selectSlotWithItem(item)
     end
     return false
 end
+--- func desc
+---@param inventory table{string}
+function confirmInventory(inventory)
+    for i = 1,16 do
+        turtle.select(i)
+        local slotInfo = turtle.getItemDetail()
+        if (inventory[i] == nil) ~= (slotInfo == nil) then
+            error("Incorrect Turtle Inventory Supplied.")
+            print(string.format("In slot %d was expecting a %s item"), i, inventory[i])
+            return false
+        end
+        if slotInfo then
+            if not string.find(slotInfo.name, inventory[i]) then
+                error("Incorrect Turtle Inventory Supplied.")
+                print(string.format("In slot %d was expecting a %s item"), i, inventory[i])
+                return false
+            end
+        end 
+    end
+    return true
+end
--- robert_projects/advanced_farmer.lua
+++ robert_projects/advanced_farmer.lua
@@ -1,62 +1,6 @@
-hoeSlot = 10
-shovelSlot = 9
 emptySlot = 11
 produceSlot = 12
-seedSlots = {1,2,3,4}
-waterSlots = {13,14,15,16}
-farmSize = 0
 
-local function confirmInventory()
-    for k,i in pairs(seedSlots) do
-        turtle.select(i)
-        local data = turtle.getItemDetail()
-        if data then
-            if not string.find(data.name, "seeds") then
-                return false
-            end
-        else 
-            return false
-        end
-    end
-
-    --Bottom inventory slots must be filled with water buckets
-    for k,i in pairs(waterSlots) do
-        turtle.select(i) 
-        local data = turtle.getItemDetail()
-        if data then 
-            if data.name ~= "minecraft:water_bucket" then
-                return false
-            end
-        else
-            return false
-        end
-    end
-    turtle.select(shovelSlot)
-    local data = turtle.getItemDetail()
-    if data then
-        if not string.find(data.name, "shovel") then
-            print(string.format("Don't forget to put a shovel in the %dth slot!", shovelSlot))
-            return false
-        end
-    else
-        print(string.format("Don't forget to put a shovel in the %dth slot!", shovelSlot))
-        return false
-    end
-
-    turtle.select(10)
-    local data = turtle.getItemDetail()
-    if data then
-        if not string.find(data.name, "hoe") then
-            print(string.format("Don't forget to put a hoe in the %dth slot!", hoeSlot))
-            return false
-        end
-    else
-        print(string.format("Don't forget to put a hoe in the %dth slot!", hoeSlot))
-        return false
-    end
-
-    return true
-end
 
 local function plantSeed()
     for k,i in pairs(seedSlots) do
@@ -98,7 +42,6 @@ end
 
 local function createFarm(size, addWater)
     function shouldPlaceWater(x, y)
-
         if size == 4 then return false end
         if x == size or x == 1 then return false end
         if y == size or y == 1 then return false end
@@ -124,47 +67,35 @@ local function createFarm(size, addWater)
         if our column number is odd, we turn right
         if it is even, we turn left   
     ]]
-    turtle.select(emptySlot) --selecting what should be an empty slot, and offloading our 
-                     --equipped tool, if there is one
-    turtle.equipRight()
-
-    turtle.select(hoeSlot) --Grab the hoe slot
+    ezTurtle.selectSlotWithItem("shovel") 
+    turtle.equipLeft()
+    ezTurtle.selectSlotWithItem("hoe") --Grab the hoe slot
     turtle.equipRight() --Equip the hoe
+    ezTurtle.selectEmptySlot()
     for x = 1,size do
         for y = 1, size do
             if shouldPlaceWater(x,y) then
-                --Put hoe away in slot 6, equip shovel from slot 5
-                --Realizing we can just do this we a right/left item equipped, oop
-                turtle.select(hoeSlot) turtle.equipRight()
-                turtle.select(shovelSlot) turtle.equipRight()
-                turtle.select(emptySlot)
                 --Dig hole under turtle
                 turtle.down()
-                turtle.digDown()
-                --Grab a bucket from bottom row that has water in it
-                for k,i in pairs(waterSlots) do
-                    turtle.select(i)
-                    bucket = turtle.getItemDetail()
-                    if string.find(bucket.name, "water") then break end
-                end
+                turtle.digDown("left")
                 --confirming we did grab a water bucket, then placing it down
-                if string.find(bucket.name, "water") then
+                if ezTurtle.selectSlotWithItem("water") then
                     turtle.placeDown()
+                else
+                    error("Ran out of water! This wasn't supposed to happen, shutting down")
+                    return
                 end
                 turtle.up()
-                --Putting shovel away, and equipping hoe
-                turtle.select(shovelSlot) turtle.equipRight()
-                turtle.select(hoeSlot) turtle.equipRight()
                 write("W ")
             else
                 --Using Hoe on Ground
-                turtle.digDown()
+                turtle.digDown("right")
                 plantSeed() 
                 write("D ")
             end
             turtle.forward()
         end
-        turtle.digDown()
+        turtle.digDown("right")
         plantSeed()
         if x % 2 == 0 then
             turtle.turnLeft()
@@ -175,77 +106,53 @@ local function createFarm(size, addWater)
             turtle.forward()
             turtle.turnRight()
         end
-        -- turtle.digDown()
-        -- plantSeed()
         write("\n")
     end
-
     --Return
     goBack(size)
-
-    turtle.select(hoeSlot)
-    turtle.equipRight()
 end
 
 local function checkFarm(size)
-    if turtle.getItemCount(hoeSlot) > 0 then
-        turtle.select(hoeSlot)
-        turtle.equipRight()
+    --Scans a data object, looking for an age-state
+   local function getAge(data)
+    if data.state then
+        if data.state.age then
+            print(string.format("This is %d age!", data.state.age))
+            return data.state.age
+        end
+        return 0
     end
-   --[[
-    Same movement style as before, column by column, 
-    turning right for odd and left for even
-   ]] 
+   end
+   --Checks the state of the plant below the turtle, harvesting it if it is grown
+   --and replacing broken/harvested blocks 
+   local function checkPlant()
+    local success, data = turtle.inspectDown()
+    if success then
+        --If our plant is fully grown
+        if getAge(data) > 6 then
+        print("Fully grown this one is!")
+        --ezTurtle.selectSlotWithItem()
+        turtle.select(produceSlot)
+        turtle.digDown("right")
+        plantSeed()
+        end
+    else
+        print("No Produce Here! Replanting...")
+        turtle.digDown("right")
+        plantSeed()
+    end
+   end
+
     for x = 1,size do
         for y = 1, size do
             --Read data about plant
-            local success, data = turtle.inspectDown()
-            local age = 0
-            if success then
-                if data.state then
-                    if data.state.age then
-                        print(string.format("This is %d age!", data.state.age))
-                        age = data.state.age
-                    end
-                end
-            else
-                print("Failed to inspect block underneat")
-                turtle.digDown()
-                plantSeed()
-            end
-            --If this plant is fully grown
-            if age > 6 then
-                print("Fully grown this one is!")
-                --ezTurtle.selectSlotWithItem()
-                turtle.select(produceSlot)
-                turtle.digDown()
-                plantSeed()
-            end
+            checkPlant()
             turtle.forward()
         end
-        --Get last block of each column
-        local success, data = turtle.inspectDown()
-        if success then
-            if data.state then
-                if data.state.age then
-                    print(string.format("This is %d age!", data.state.age))
-                    age = data.state.age
-                end
-            end
-        else
-            print("Failed to inspect block underneat")
-            turtle.digDown()
-            plantSeed()
-        end
-        --If this plant is fully grown
-        if age > 6 then
-            print("Fully grown this one is!")
-            --ezTurtle.selectSlotWithItem()
-            turtle.select(produceSlot)
-            turtle.digDown()
-            plantSeed()
-        end
+        --We call this to check the last plant in each column
+        checkPlant()
 
+        --Cycling to next row, turning around as well
         if x % 2 == 0 then
             turtle.turnLeft()
             turtle.forward()
@@ -256,33 +163,31 @@ local function checkFarm(size)
             turtle.turnRight()
         end
     end
-    turtle.select(hoeSlot) turtle.equipRight()
     goBack(size)
 end
-local args = {...}
 
-if args[1] ~= "inspectOnly" then
-if confirmInventory() then
+local neededInventory = {"seeds", "seeds", "seeds", "seeds", 
+                          nil,     nil,     nil,     nil, 
+                          "shovel", "hoe",  nil,     nil,
+                          "water", "water", "water", "water"    
+                        }
+
+
+if ezTurtle.confirmInventory(neededInventory) then
     io.write("Great! Your inventory is set up, I will begin now. Please give me space to work!\n",
         "What size should the farm be?\nSize: ")
     local size = tonumber(io.read())
-    farmSize = size
     io.write(string.format("Can you confirm that there is a %d x %d size area in front of me?", size, size)) 
     local confirm = io.read()
     if string.find(string.lower(confirm), "yes") then
         print("Ok, building!")
-        createFarm(farmSize, true)
+        createFarm(size, true)
         print("Farm Created!! Doing Inspections every 60 seconds")
         while true do
             sleep(60)
-            checkFarm(farmSize)
+            checkFarm(size)
         end
     end 
 else
     print("Sorry, I can't start yet! My invetory needs seeds to fill the top row, and water buckets to fill the bottom")
-end
-else
-   print "How big is the farm?"
-   local size = tonumber(io.read())
-   checkFarm(size)
 end
