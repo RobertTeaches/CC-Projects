8ab5de1ae8dc4e0f1d5c5d98be565e1705b05d78
RobertTeaches <77590088+RobertTeaches@users.noreply.github.com>
2022-08-02 02:36:02 -0400

    Big boy updates to apis and proejcts

--- robert_projects/ui/shop_menu.lua
+++ robert_projects/ui/shop_menu.lua
@@ -1,47 +1,129 @@
-ui = require("ui")
+local ui = require("ui")
+local shopMonitor = peripheral.wrap("back")
+local workerDisplay = peripheral.wrap("right")
+--[[
+    This project has a few differnt ways to teach it, depending on the guide-writer's preference
+    Our options:
+    1 - Auto Grid OR Hand-Placed Items. 
+        Auto grid offloads the spacing and placement of buttons to the UI api, which may or may not be ideal 
+        for this project difficulty. Hand-Placing can get tedious, but teaches the fundamentals of screen space 
+        more literally
+
+    2 - Hardcoding the Menu OR Loading it from a .txt File
+        Hardcoding the menu reduces overhead, and has them interacting with table implicit declarations
+        Loading it from a textfile adds concepts such as: 
+            File-Opening, Closing, and General Management
+            Lua's string.gmatch(), which does pattern matching similar to Regex
+            Shell directory understanding, and filepath understanding
+
+    3 - Extra monitor for Employee OR Print to terminal
+        Depending on the complexity and scope of this project, you may want to skip the extra monitor and 
+        just print the shop-results to the terminal. This can reduce logistics and explanation overhead, as you
+        don't have to manage two monitors, and can take advantage of peripheral.find()
+
+
+]]--
 
 
 --Print out Shopping Results on Store Computer
 local function printToWorker(phrase)
+    local old = term.redirect(workerDisplay)
     print(phrase)
+    term.redirect(old)
 end
+
+--HARDCODED Menu
 --Name,Price
 local menu = {
     {"Sword", 5},
-    {"Shovel", 2},
+    {"Shovel", 8},
     {"Axe" , 4},
     {"Bow" , 2},
 }
 
 
---Show shop UI 
+--Show shop UI with hand-picked values
 local function shopUi()
-    local monitor = peripheral.find("monitor")
-    if not monitor then error "Need a Monitor to Display On!" end
-    window = ui.createMainWindow(monitor)
+    if not shopMonitor then error "Need a Monitor to Display On!" end
+    shopWindow = ui.createMainWindow(shopMonitor)
     width,height = ui.getSize()
-    ui.createButton("Test", width/2, height/2)
+
+    ui.createButton("X", width - 1, 1, 2, 1, colors.white, colors.red, "exit")
+    ui.createButton("Sword", width/4, height/4, 7, 3)
+    ui.createLabel("5", width/4 + 2, (height/4) - 1, 3, colors.black, colors.yellow)
+
+    ui.createButton("Shovel", 8+ (width/4), height/4, 8, 3)
+    ui.createLabel("5", 10 + (width/4) , (height/4) - 1, 4, colors.black, colors.yellow)
+
+    ui.drawAll()
+end
+
+---Loads a menu with "Item price" configuration, line by line
+---@param path string
+---@return table
+local function loadMenuFromTxt(path)
+    if not fs.exists(path) then fs.open(path, "w") end
+    local file = fs.open(path, "r")
+    local m = {}
+
+    line = file.readLine()
+    while line do 
+        local item = {}
+        for token in string.gmatch(line, "[^%s^]+") do
+            table.insert(item,token)
+        end
+        table.insert(m, item)
+        line = file.readLine()
+    end
+
+    return m
+end
+
+--Show shop UI in grid using UI.createButtonsInGrid() function
+local function shopUIGrid()
+    if not shopMonitor then error "Need a Monitor to Display On!" end
+    shopWindow = ui.createMainWindow(shopMonitor)
+    width,height = ui.getSize()
+    menu = loadMenuFromTxt (shell.dir().."/prices.txt")
+    local text = {}
+    for  _,item in pairs(menu) do
+        table.insert(text, item[1]..":"..tostring(item[2]))
+    end
+
+    ui.createButtonsInGrid(text, 2, 10, 3, nil, nil, vector.new(3, 0))
+    ui.createButton("X", width - 1, 1, 2, 1, colors.white, colors.red, "exit")
+    ui.createButton("@", 1, 1, 2, 1, colors.white, colors.blue, "reset")
     ui.drawButtons()
 end
 
 local function monitorTouch(x,y)
-    window.setCursorPos(1,1)
-    local b = ui.checkButtonClick(x,y,window)
+    local b = ui.checkButtonClick(x,y,shopWindow)
     if b then 
+        if b == "exit" then
+            ui.clear(colors.lightBlue)
+            workerDisplay.clear()
+            error ""
+        elseif b == "reset" then
+            ui.clear(colors.black)
+            workerDisplay.clear()
+            shell.run("shop_menu.lua")
+            error ""
+        else
         local name,price = table.unpack(menu[b])
         printToWorker(name..":"..tostring(price))
+        end
     end
 end
 
 local function monitorControl()
     while true do
         m, side, x, y = os.pullEvent("monitor_touch")
-        window.setCursorPos(1,1)
+        shopWindow.setCursorPos(1,1)
         --for i,v in pairs(m) do print(i,v) end
         --print(x, y)
         monitorTouch(x,y) 
     end
 end
 
-shopUi()
+shopUIGrid()
 monitorControl()
--- robert_projects/ui/ui.lua
+++ robert_projects/ui/ui.lua
@@ -2,18 +2,18 @@ buttons = {}
 labels = {}
 mainWindow = nil
 width,height = 51,19
-windowColor = colors.black
+local windowColor = colors.black
 
 --- creates a 'button' ui object to be dislpayed in the window. Does not automatically draw the buttons, must use drawButtons()
 ---@param buttonText string
 ---@param x number
 ---@param y number
----@param width number or nil
----@param height number or nil
----@param foreColor color or nil
----@param backColor color or nil
----@param buttonID any or nil
----@param window table or nil
+---@param width number | nil
+---@param height number | nil
+---@param foreColor color | nil
+---@param backColor color |nil
+---@param buttonID any | nil
+---@param window table | nil
 function createButton(buttonText,x,y, width, height, foreColor, backColor,buttonID, window)
 
     --Setting defaults, probably not needed in project-guide
@@ -46,7 +46,7 @@ function createButton(buttonText,x,y, width, height, foreColor, backColor,button
         for i = 1, afterNu do buttonText = buttonText.." " end
     end
 
-
+    --The basis for ALL button object
     local buttonObject = {
         text = buttonText,
         position = vector.new(x,y),
@@ -59,6 +59,103 @@ function createButton(buttonText,x,y, width, height, foreColor, backColor,button
     window.setBackgroundColor(windowColor)
 end
 
+---Creates and places a grid in a window, fitting content or parameters
+---@param text {any:string}
+---@param buttonWidth number|nil
+---@param buttonHeight number|nil
+---@param spacing number|nil
+---@param window craftosWindow|nil
+---@param gridWidth number|nil
+---@param offset vector|nil
+---@param buttonIDPrefix string|nil
+function createButtonsInGrid(text, spacing, buttonWidth, buttonHeight,  window, gridWidth, offset, buttonIDPrefix)
+
+    local longestWord = 0
+    for i,t in pairs(text) do 
+        if string.len(t) > longestWord then longestWord = string.len(t) end
+    end
+    local buttonWidth , buttonHeight = buttonWidth or longestWord , buttonHeight or 3
+    spacing = spacing or 1
+    gridWidth = gridWidth or width
+    offset = offset or vector.new(0,0)
+    local numOfColumns = math.min( math.floor(gridWidth / (buttonWidth + spacing*2)), #text)
+    local numOfRows = math.ceil(#text / numOfColumns)
+    local totalColumnWidth = numOfColumns * (buttonWidth + spacing)
+    local totalRowHeight = numOfRows * (buttonHeight + spacing)
+    local startPoint = vector.new((width - totalColumnWidth)/2, ((height - totalRowHeight)/2) + buttonHeight/2) + offset
+    
+    local i = 1
+    for y = 0, numOfRows-1 do
+        for x = 0, numOfColumns-1 do
+            local bPos = vector.new(x * (buttonWidth + spacing) + startPoint.x,
+                                     y * (buttonHeight + spacing) + startPoint.y)
+            if string.len(text[i]) > buttonWidth then text[i] = string.sub(text[i], 1, buttonWidth) end
+            local id = (buttonIDPrefix and buttonIDPrefix..tostring(i)) or id
+            createButton(text[i], bPos.x, bPos.y, buttonWidth, buttonHeight, 
+                            colors.white, colors.gray, id, window)
+            i = i + 1
+            if i > #text then return end 
+        end
+    end
+end
+
+--Creates a ui object for displaying lines of text. Can only handle SINGLE lines, with no word-wrapping
+---@param labelText string
+---@param x number
+---@param y number
+---@param width number|nil
+---@param foreColor color|nil
+---@param backColor color|nil
+---@param labelID any |nil
+---@param window table | nil
+function createLabel(labelText, x,y, width, foreColor, backColor, labelID, window)
+    window = window or mainWindow
+    labelID = labelID or #labels + 1
+    x = x or 1
+    y = y or 1
+    --Caching len of text
+    local len = string.len(labelText)
+    width = width or len
+    foreColor = foreColor or colors.white
+    backColor = backColor or colors.gray
+
+    local wDelta = width - len
+    if wDelta >= 0 then
+        local beforeSpaces = math.floor(wDelta/2)
+        local afterSpaces = math.ceil(wDelta/2)
+        --Adding proper spacing before and after
+        for i = 1,beforeSpaces do labelText = " "..labelText end
+        for i = 1,afterSpaces do labelText = labelText.." " end
+    else --If our label text is bigger than width, we override the provided value
+        width = len
+    end
+
+    --This is the basis for all labels in the UI API, and their fields
+    local labelObject = {
+        text = labelText,
+        position = vector.new(x,y),
+        width = width,
+        textColor = foreColor,
+        backColor = backColor,
+        parentWindow = window
+    }
+    table.insert(labels, labelObject)
+end
+
+
+function drawLabels()
+    for i,label in pairs(labels) do
+        local blitFColor, blitBColor = "",""
+        local x,y = label.position.x, label.position.y
+        local window = label.parentWindow
+        for x = 1,string.len(label.text) do
+            blitBColor = blitBColor..colors.toBlit(label.backColor)
+            blitFColor = blitFColor..colors.toBlit(label.textColor)
+        end
+        window.setCursorPos(x,y)
+        window.blit(label.text, blitFColor, blitBColor)
+    end
+end
 
 function drawButtons()
     for i,button in pairs(buttons) do
@@ -72,16 +169,22 @@ function drawButtons()
             bTColor = bTColor..tostring(colors.toBlit(button.foreColor))
             bBColor = bBColor..tostring(colors.toBlit(button.backColor))
         end
-        --print(string.len(buttonText), bTColor, string.len(bBColor))
+        ------We need to redirect for paintutils, as it uses term.current() for it's window 
         local old = term.redirect(window)
         paintutils.drawFilledBox(x, y, x + width - 1, y + height - 1, button.backColor)
         term.redirect(old)
+        ---------------------------------------------------------------------------
         window.setCursorPos(x,y + math.floor(height/2))
-        window.setBackgroundColor(button.backColor)
-        window.setTextColor(button.foreColor)
+        --window.setBackgroundColor(button.backColor)
+        --window.setTextColor(button.foreColor)
         window.blit(button.text, bTColor, bBColor)
     end
-    window.setBackgroundColor(windowColor)
+    shopWindow.setBackgroundColor(windowColor)
+end
+
+function drawAll()
+    drawButtons()
+    drawLabels()
 end
 
 function checkButtonClick(x,y, window)
@@ -105,9 +208,14 @@ function checkButtonClick(x,y, window)
     return false
 end
 
+function setBackgroundColor(color)
+    if not color then return end
+    mainWindow.setBackgroundColor(color)
+end
+
 function clear(color)
     color = color or windowColor
-    mainWindow.setBackgroundColor(color)
+    setBackgroundColor(color)
     mainWindow.clear()
 end
 
@@ -124,5 +232,7 @@ function getSize()
     return width,height
 end
 --For 'require' implementations
-return {buttons = buttons, mainWindow = mainWindow, getSize = getSize, windowColor = windowColor,
-        createButton = createButton, drawButtons = drawButtons, checkButtonClick = checkButtonClick, clear = clear, createMainWindow = createMainWindow}
+return {buttons = buttons, mainWindow = mainWindow, getSize = getSize, createButton = createButton, 
+        createButtonsInGrid = createButtonsInGrid,drawButtons = drawButtons, checkButtonClick = checkButtonClick, 
+        clear = clear, createMainWindow = createMainWindow, createLabel = createLabel, drawLabels = drawLabels,
+        drawAll = drawAll}
