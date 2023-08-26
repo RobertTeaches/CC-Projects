3bef84370c6c9115ca12394fa5ecb95575a275f5
Robert Clark <77590088+RobertTeaches@users.noreply.github.com>
2023-08-26 11:14:14 -0400

    Add files via upload

--- robert_projects/pong_server.lua
+++ robert_projects/pong_server.lua
@@ -17,11 +17,11 @@ local playerSize = vector.new(1, 3)
 local gameSize = vector.new(52, 19)
 
 local ballPos = vector.new(gameSize.x / 2, gameSize.y / 2)
-local ballVel = vector.new(0.1, 0.1)
+local ballVel = vector.new(1, 1)
 
 local playGame = true
 --Networking variables
-local refresh_rate = 1/5
+local refresh_rate = 1 / 10
 
 local recievingChannel = 112
 
@@ -44,7 +44,7 @@ local function connect_players()
         until channel == recievingChannel
     end
     --We received the connect message from a computer
-    modem.transmit(replyChannel, recievingChannel, textutils.serialiseJSON({"player1", player1Channel}))
+    modem.transmit(replyChannel, recievingChannel, textutils.serialiseJSON({ "player1", player1Channel }))
     message = ""
     print("Player 1 Connected")
     while message ~= "connect" do
@@ -52,44 +52,67 @@ local function connect_players()
             event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
         until channel == recievingChannel
     end
-    modem.transmit(replyChannel, recievingChannel, textutils.serialiseJSON({"player2", player2Channel}))
+    modem.transmit(replyChannel, recievingChannel, textutils.serialiseJSON({ "player2", player2Channel }))
 
     return true
 end
 
 local function update_clients()
-    modem.transmit(player1Channel, textutils.serialiseJSON({player1.position, ballPos}))
-    modem.transmit(player2Channel, textutils.serialiseJSON({player2.position, ballPos}))
+    modem.transmit(player1Channel, recievingChannel, textutils.serialiseJSON({ player2.position, ballPos, ballVel }))
+    modem.transmit(player2Channel, recievingChannel, textutils.serialiseJSON({ player1.position, ballPos, ballVel }))
 end
 
 local function recieve_client()
-    --Payload will be in JSON array, with
-    -- 1. being player sending
-    -- 2. being playerPos
-    repeat
-        event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-    until channel == recievingChannel
-    local payload = textutils.unserialiseJSON(message)
-    if payload then
-        if payload[1] == "player1" then
-            player1.position = payload[2]
-        else
-            player2.position = payload[2]
+    while playGame do
+        --Payload will be in JSON array, with
+        -- 1. being player sending
+        -- 2. being playerPos
+        repeat
+            event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+        until channel == recievingChannel
+        print(message)
+        local payload = textutils.unserialiseJSON(message)
+        if payload then
+            if payload[1] == "player1" then
+                player1.position = vector.new(payload[2].x, payload[2].y)
+                print("Player 1: "..player1.position.y)
+            else
+                player2.position = vector.new(payload[2].x, payload[2].y)
+                print("Player 2: "..player2.position.y)
+            end
         end
     end
 end
 
-local function ball_collision(playerPos)
+local function reset_ball()
+        ballVel = vector.new(math.random(1) - 0.5, math.random(1) - 0.5)
+        ballPos = vector.new(gameSize.x / 2, gameSize.y / 2)
+end
 
+local function ball_collision()
+    if ballPos.x > gameSize.x or ballPos.x < 1 then
+        reset_ball()
+    end
+    if ballPos.y > gameSize.y or ballPos.y < 1 then
+        ballVel.y = -ballVel.y
+    end
+    if ballPos.x == player1.position.x and ballPos.y >= player1.position.y and ballPos.y <= player1.position.y + playerSize.y then
+        ballVel.x = -ballVel.x * 1.1
+    end
+    if ballPos.x == player2.position.x and ballPos.y >= player2.position.y and ballPos.y <= player2.position.y + playerSize.y then
+        ballVel.x = -ballVel.x * 1.1
+    end
 end
 
 local function game_logic()
     while playGame do
         sleep(refresh_rate)
-        
+        update_clients()
+        ballPos = ballPos + ballVel
+        ball_collision()
     end
 end
 
-connect_players()
+parallel.waitForAny(connect_players, recieve_client)
 
 parallel.waitForAll(game_logic, recieve_client)
